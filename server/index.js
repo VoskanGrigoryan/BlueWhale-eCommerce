27b8372a199992d51b1aec4fcda048e8a2f254a1
VoskanGrigoryan <voskan.grigoryan.arg@gmail.com>
2021-08-04 00:26:45 -0300

    test

---
+++ server/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+const conf = dotenv.config();
+const __dirname = path.resolve(path.dirname(''));
+const PORT = process.env.PORT || 4000;
+const DB_CONNECTION_URL = `mongodb+srv://VoskanGrigoryan:bLZAxc0fp132@cluster0.qb578.mongodb.net/eCommerce`;
+
+app.use(compression());
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+
+try {
+    mongoose
+        .connect(
+            DB_CONNECTION_URL,
+            {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useUnifiedTopology: true,
+            },
+            console.log('Connected to db')
+        )
+        .then(() => {
+            app.listen(PORT, console.log(`Running on port ${PORT}`));
+        })
+        .catch((err) => console.log(err));
+} catch (error) {
+    console.log(error);
+}
